413f86346c69cdc2bd566e17b2e8ad3ae541e36d
Dharnishan <dharnishan@gmail.com>
2025-02-25 02:44:02 +0530

    changed

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,7 +1,7 @@
 import ResumeImg from '../Assests/resume.jpg';
 export default function Resume(){
     const config = {
-        link: 'src\files\Dharnishan Manoharan.pdf'
+        link: 'src\files\DharnishanManoharan.pdf'
     }
     return <section id='resume' className='flex flec-col md:flex-row flex bg-primary px-5'>
         <div className='py-5 md:w-1/2 flex justify-end'>
@@ -10,7 +10,7 @@ export default function Resume(){
         <div className='px-20 w-1/2 flex justify-center'>
             <div className='flex flex-col justify-center'>
                 <b><h1 className='text-4xl text-white border-b-4 mb-4 w-[135px]'>Resume</h1></b>
-                <p className='text-white'>You can view my resume <a className='btn' href={config.link} download="Dharnishan Manoharan.pdf">Download</a> </p>
+                <p className='text-white'>You can view my resume <a className='btn' href={config.link} download="DharnishanManoharan.pdf">Download</a> </p>
             </div>
             
         </div>
